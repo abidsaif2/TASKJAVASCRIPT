c37b937f27e77e4e677d50bc19cbb32149ac6a18
USERNAME <MY_NAME@example.com>
2021-03-05 15:58:31 +0100

    Some HTML , CSS changing

--- app.js
+++ app.js
@@ -6,25 +6,31 @@ nt.innerHTML='JAVASCRIPT TASK BY ABID SAIF';
 var ft = document.querySelector("#footer_title");
 ft.innerHTML='Copyright ABID SAIF';
 
-const productList = document.querySelector('#card_articles');
+ const productList = document.querySelector('#card_articles');
+
+//  supprimer des articles
+
+
 
 // load product items content from JSON File
 
-fetch("products.json")
-  .then(response => response.json())
-  .then(data => {
-    let html = '';
-      data.forEach(product => {
-        html += `
-      <span id="card">
-        <div id="id"></div>
-        <div id="img"><img src="${product.image}"></div>
-        <div id="nom">${product.name}</div>
-        <div id="category">${product.category}</div>
-        <div id="color">${product.color}</div>
-        <div id="size">${product.size}</div>
-        <div id="price">${product.price}$</div>
-     </span>  `;
-      });
-       productList.innerHTML = html;
-  })
+ fetch("products.json")
+   .then(response => response.json())
+   .then(data => {
+     let html = '';
+       data.forEach(product => {
+         html += `
+       <span id="card">
+         <div id="id">${product.id}</div>
+         <div id="img"><img src="${product.image}"></div>
+         <div id="nom">${product.name}</div>
+         <div id="category">${product.category}</div>
+         <div id="color">${product.color}</div>
+         <div id="size">${product.size}</div>
+         <div id="price">${product.price}$</div>
+         <div id="edit">edit</div>
+         <div id="delete">delete</div>
+      </span>  `;
+       });
+        productList.innerHTML = html;
+   })
