fcee7d49364b2a954eebf745640fa0004f270985
USERNAME <MY_NAME@example.com>
2021-03-05 15:07:26 +0100

    Implementation des articles dans des card avec DOM avec l'utilisation du Fichier JSON, TASK 1 Terminer

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-// Récuperation des sous ID et l'implémentation des textes , navbar et footer
+// Récuperation des sous ID et l'implémentation des textes , navbar et footer , card
 
 var nt = document.querySelector("#nav_title");
 nt.innerHTML='JAVASCRIPT TASK BY ABID SAIF';
 
 var ft = document.querySelector("#footer_title");
 ft.innerHTML='Copyright ABID SAIF';
+
+const productList = document.querySelector('#card_articles');
+
+// load product items content from JSON File
+
+fetch("products.json")
+  .then(response => response.json())
+  .then(data => {
+    let html = '';
+      data.forEach(product => {
+        html += `
+      <span id="card">
+        <div id="id"></div>
+        <div id="img"><img src="${product.image}"></div>
+        <div id="nom">${product.name}</div>
+        <div id="category">${product.category}</div>
+        <div id="color">${product.color}</div>
+        <div id="size">${product.size}</div>
+        <div id="price">${product.price}$</div>
+     </span>  `;
+      });
+       productList.innerHTML = html;
+  })
