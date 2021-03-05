4d126236088cfee66e1d86d9c86c54cba3e88286
USERNAME <MY_NAME@example.com>
2021-03-05 11:53:21 +0100

    NAVBAR & Footer Avec DOM

---
+++ app.js
@@ -0,0 +1,8 @@
+var nav = document.getElementById("nav");
+var footer = document.getElementById("footer");
+
+var nt = document.querySelector("#nav_title");
+nt.innerHTML='JAVASCRIPT TASK BY ABID SAIF';
+
+var ft = document.querySelector("#footer_title");
+ft.innerHTML='Copyright ABID SAIF';
